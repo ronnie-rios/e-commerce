b9ee71c1e0bc3656a326f9baa43565d67744eb61
Ronnie Rios <ronniesmail14@gmail.com>
2021-06-17 14:34:19 -0500

    added get findall and find one functions to product routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,51 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+       { 
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+        through: ProductTag,
+        as: 'tags'
+       }
+    ]
+  }).then(dbProduct => res.json(dbProduct))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+       { 
+        model: Tag,
+        attributes: ['tag_name'],
+        through: ProductTag,
+        as: 'tags'
+       }
+    ]
+      }).then(dbProduct => res.json(dbProduct))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 // create new product
